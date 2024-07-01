b0c6b19693c301f78b3546149c3ff50690c9c012
vladislavkotsko <vlad.kotsko.github@gmail.com>
2024-07-02 00:47:15 +0300

    12345

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -1,5 +1,7 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello everybody");
+        System.out.println(1);
     }
 }
